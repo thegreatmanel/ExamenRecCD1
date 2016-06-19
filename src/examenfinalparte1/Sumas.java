b4137ae89abc8711b8e5f0c45f58e88a29740fd0
propa <propa@DESKTOP-10ADTBO>
2016-06-19 21:33:27 +0200

    JavaDoc añadido

--- src/examenfinalparte1/Sumas.java
+++ src/examenfinalparte1/Sumas.java
@@ -1,5 +1,10 @@
 package examenfinalparte1;
 
+/**
+ * esta clase si recibe un numero el atributo resultado almacenará el doble,
+ * si recibe dos números devuelve la suma, y si no recibe números devolverá -1
+ * @author propa
+ */
 public class Sumas {
 
     private int resultado;
@@ -20,7 +25,13 @@ public class Sumas {
     public Sumas() {
         resultado = -1;
     }
-
+    
+    /**
+     * Devuelve la suma de los números recibidos
+     * @param numero1 Un operando entero
+     * @param numero2 Un operando entero
+     * @return 
+     */
     public int sumar(int numero1, int numero2) {
         return numero1 + numero2;
     }
